4e578f841c84175fa8dc95de23e4a52dd3550d8d
Callum Leahy <callumleahy23@googlemail.com>
2016-09-20 13:54:09 +0100

    added routes relying on controllers

---
+++ config/routes.js
@@ -0,0 +1,18 @@
+var express = require('express');
+//using just the Router constructor in express
+var filmRouter = express.Router();
+
+//requiring the film controller
+var filmController = require('../controllers/films');
+
+filmRouter.get('/films/new',filmController.new);
+filmRouter.route('/films')
+	.get(filmController.index)
+	.post(filmController.create);
+filmRouter.route('/films/:id')
+	.get(filmController.show)
+	.patch(filmController.update)
+	.delete(filmController.delete);
+filmRouter.get("films/:id/edit",filmController.edit);
+
+module.exports = filmRouter;
